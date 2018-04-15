f19c2a6b5a60a8ea6c0131cdfd92fc00d8789df5
Abel Meneses-Abad <abelma1980@gmail.com>
2018-04-15 13:24:26 -0400

    [dev] changing structure of process script to make the whole tfidf model loaded, executed and all model-correspondent measures applied to 100 pair of sentences of MSRP corpus. {IT WORKS} [doc][support] some little changes of jpynb to normalize structure.

--- preprocess.py
+++ preprocess.py
@@ -9,14 +9,14 @@ from configparser import ConfigParser
 import os, sys
 import pandas as pd
 
-from gensim.models import TfidfModel
+from gensim.models import TfidfModel, Word2Vec, Doc2Vec
 from gensim.corpora import TextCorpus, MmCorpus, Dictionary
 
 import numpy as np
 
 #Importing all features
 from sklearn.metrics.pairwise import cosine_similarity as kcosine
-from textsim.tokendist import cosine_distance_scipy as scosine
+from textsim.tokendists import cosine_distance_scipy as scosine
 from gensim.matutils import cossim as gcosine
 from gensim.matutils import jaccard as gjaccard
 from distances import n_similarity as nsimilarity
@@ -32,84 +32,89 @@ from distances import harmonic_best_pair_word_sim as hbestpw
                         words in both sentences.)
 '''
 
-class Model:
+#SIM_VECTOR
+features = {
+    'gcosine':gcosine,
+    'gjaccard':gjaccard,
+    'scosine':scosine,
+    'kcosine':kcosine,
+    'nsimilarity':nsimilarity,
+    'jonh2016':jonh2016,
+    'hbestpw':hbestpw
+}
+columns=list(features.keys())
 
-    def __init__(self,model,corpus):
-        #Please modify your all the paths for your resources
-        print('Modify the paths of your corpus on config.ini file')
-        input()
+def init_model(model_type,corpus):
+    #Please modify your all the paths for your resources
+    print('Modify the paths of your corpus on config.ini file')
+    corpus = corpus
 
-        self.name = model
-        self.corpus = corpus
-        #SIM_VECTOR
-        self.features = {
-            'gcosine':gcosine,
-            'gjaccard':gjaccard,
-            'scosine':scosine,
-            'kcosine':kcosine,
-            'nsimilarity':nsimilarity,
-            'jonh2016':jonh2016,
-            'hbestpw':hbestpw
-        }
-        self.columns=list(self.features.keys())
-
-        config = ConfigParser()
-        config.read('config.ini')
-
-        '''TODO: Generalize this step puting corpus_path as your actual corpus
+    config = ConfigParser()
+    config.read('config.ini')
+    '''TODO: Generalize this step puting corpus_path as your actual corpus
         Config file must allow wikipedia, Gutenberg, ...'''
-        corpus_path = config['WIKI']['en'][1:-1]
-        self.dictionary, self.bow_corpus = self.load_corpus() 
-        self.tfidf = self.generate_corpus()
-
-    def load_corpus(self):
-        self.dictionary = Dictionary.load_from_text(os.path.relpath(corpus_path+'_wordids.txt.bz2'))
-        self.bow_corpus = MmCorpus(os.path.relpath(corpus_path+'_bow.mm'))
-        return self.dictionary, self.tfidf
-
-    def generate_corpus(self):
-        try:
-            self.tfidf = TfidfModel.load(corpus_path+'wiki-tfidf.model')
-        except:
-            self.tfidf = TfidfModel()
-            self.tfidf = TfidfModel(bow_corpus,dictionary)
-            self.tfidf._smart_save(corpus_path+'wiki-tfidf.model')
+    if 'wiki' in corpus:
+        if 'tfidf' in model_type:
+            corpus_path = config['WIKI']['en'][2:-1]
+            dictionary = Dictionary.load_from_text(os.path.relpath(corpus_path+'_wordids.txt.bz2'))
+            bow_corpus = MmCorpus(os.path.relpath(corpus_path+'_bow.mm'))
+            model = generate_model(dictionary,bow_corpus, corpus_path)
+            return model, dictionary, bow_corpus
+        else:
+            print('Only tfidf is supported')
             pass
-        return self.tfidf
+    else:
+        print('Only wiki corpus is supported')
+        pass
+
+def generate_model(dictionary,bow_corpus,corpus_path):
+    try:
+        tfidf = TfidfModel.load(corpus_path+'wiki-tfidf.model')
+        print('tfidf model generated')
+    except:
+        tfidf = TfidfModel()
+        tfidf = TfidfModel(bow_corpus,dictionary)
+        tfidf._smart_save(corpus_path+'wiki-tfidf.model')
+        pass
+    return tfidf
 
 class Pair:
-    def __init__ (self, sentence1, sentence2, model):
+    def __init__ (self, sentence1, sentence2, model, dictionary, bow_corpus):
         
-        self.model = model.lower()
+        self.model = model
+        self.dictionary = dictionary
+        self.bow_corpus = bow_corpus
 
         #Transforming sentences
         self.sent1 = sentence1.split()
         self.sent2 = sentence2.split()
 
-        self.bow_sent1 = dictionary.doc2bow(self.sent1)
-        self.bow_sent2 = dictionary.doc2bow(self.sent2)
+        self.bow_sent1 = self.dictionary.doc2bow(self.sent1)
+        self.bow_sent2 = self.dictionary.doc2bow(self.sent2)
 
-        self.bow1_tfidf = tfidf_bow_vector(self.bow_sent1)
-        self.bow2_tfidf = tfidf_bow_vector(self.bow_sent2)
+        self.bow1_tfidf = self.tfidf_bow_vector(self.bow_sent1)
+        self.bow2_tfidf = self.tfidf_bow_vector(self.bow_sent2)
 
-        self.nvec1, 
-        self.nvec2 = bowvec_to_npvec(self.bow1_tfidf, self.bow2_tfidf)
+        self.nvec1, self.nvec2 = self.bowvec_to_npvec(self.bow1_tfidf, self.bow2_tfidf)
 
-        self.fvec1 = fvec(self.nvec1)
-        self.fvec2 = fvec(self.nvec2)
+        self.fvec1 = self.nvec1.reshape(1,-1)
+        self.fvec2 = self.nvec2.reshape(1,-1)
 
     def tfidf_bow_vector(self, bow_sent):
-        if self.model == 'tfidf':
-            return(tfidf[bow_sent])
+        print(type(self.model))
+        if isinstance(self.model, TfidfModel):
+            print('reconociendo el TfIdfModel')
+            return self.model[bow_sent]
         else:
+            print('entrando al else')
             return False
 
     def bowvec_to_npvec(self, bow1_tfidf,bow2_tfidf):
         #from bowvec to numerical list sent
         nvec1 = []
         nvec2 = []
-        vec1 = dict(bow1_tfidf)
-        vec2 = dict(bow2_tfidf)
+        vec1 = dict(self.bow1_tfidf)
+        vec2 = dict(self.bow2_tfidf)
         words = set(vec1.keys()).union(vec2.keys())
         for word in words:
             nvec1.append(vec1.get(word,0.0))
@@ -130,13 +135,13 @@ def apply_all_sim(pair,model):
     row = []
     for feature in features:
         if feature in ['kcosine']:
-            row.append(features[feature](pair.fvec1,pair.fvec2))
-        if feature in ['scosine']:
+            row.append(features[feature](pair.fvec1,pair.fvec2)[0][0])
+        elif feature in ['scosine']:
             row.append(features[feature](pair.nvec1,pair.nvec2))
-        if feature in ['gcosine','gjaccard']:
+        elif feature in ['gcosine','gjaccard']:
             row.append(features[feature](pair.bow1_tfidf,pair.bow2_tfidf))
-        if model.name not in ['w2v','p2v']:
-            pass
+        elif isinstance(model,Doc2Vec) or isinstance(model, Word2Vec):
+            pass #here implement the exclusive similarities of this methods
         else:
             row.append('NaN')
     return row
@@ -144,29 +149,31 @@ def apply_all_sim(pair,model):
 if __name__ == '__main__':
 
     if len(sys.argv) == 5:
-        indata = sys.argv[2]
-        model = sys.argv[3]
-        corpus = sys.argv[4]
-        outdata = sys.argv[5]
+        indata = sys.argv[1]
+        model_type = sys.argv[2]
+        corpus = sys.argv[3]
+        outdata = sys.argv[4]
 
         #input sentences
-        data = read_csv('data/'+indata,sep='\t',header=0)
+        data = pd.read_csv('data/'+indata,sep='\t',header=0)
 
         #Initializing the model
-        model = Model(model,corpus)
+        model,dictionary,bow_corpus = init_model(model_type,corpus)
 
         #declaring ouput DataFrame
-        output = pd.DataFrame(columns=model.columns)
+        output = pd.DataFrame(columns=columns)
 
         #measure the first 100 sentence pairs
         for i in range(100):
             sentence1 = data.sentence1[i]
             sentence2 = data.sentence2[i]
             
-            pair = Pair(sentence1,sentence2)
+            pair = Pair(sentence1,sentence2, model, dictionary, bow_corpus)
             
             sim_vec = []
             sim_vec = apply_all_sim(pair, model)
+            print(len(features))
+            print(len(sim_vec),sim_vec)
             output.loc[i] = sim_vec
 
         output.to_csv('data/'+outdata+'.csv')
